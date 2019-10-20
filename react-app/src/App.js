d9372ed40d0554e50cc24f0f5e84be8c36bf6088
Roshini Saravanakumar <roshini2@illinois.edu>
2019-10-20 13:05:10 -0500

    Added basic UI + bootstrap

--- react-app/src/App.js
+++ react-app/src/App.js
@@ -1,23 +1,28 @@
 import React from 'react';
 import logo from './logo.svg';
 import './App.css';
+import 'bootstrap/dist/css/bootstrap.min.css';
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://auth.illinifoodies.xyz/login?response_type=token&client_id=2h8u013ovbmseaaurir8981hcs&redirect_uri=https://illinifoodies.xyz"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+      <nav className="navbar navbar-expand-md navbar-dark bg-react-dark pt-3">
+        <a className="navbar-brand" href="#"><img className="logo mr-4" src={require("./images/logo.png")}></img>Illini Foodies</a>
+        <ul className="navbar-nav ml-auto">
+          <li className="nav-item active"><a className="nav-link" href="#">Home</a></li>
+          <li className="nav-item"><a className="nav-link" href="#">Contact</a></li>
+          <li className="nav-item"><a className="nav-link" href="#">Log In</a></li>
+          <li className="nav-item"><button className="nav-link btn btn-outline-dark ml-2" href="#">Sign Up</button></li>
+        </ul>
+      </nav>
+
+      <header className="jumbotron foodie-header">
+        <form className="form-inline justify-content-center align-items-end h-50">
+          <div className="input-group">
+            <input type="text" className="form-control" placeholder="Find food near"></input>
+            <input type="text" className="form-control" placeholder="tags (price, cuisine, i'm broke and hangry, etc.)"></input>
+          </div>
+        </form>
       </header>
     </div>
   );
