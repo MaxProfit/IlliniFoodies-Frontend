791acd5d49c38c47acf6eade5a1611fd103eaeb5
Roshini Saravanakumar <roshini2@illinois.edu>
2019-10-30 21:32:58 -0500

    Updated pages + style libraries + demo page

--- react-app/src/App.js
+++ react-app/src/App.js
@@ -3,56 +3,52 @@ import logo from "./logo.svg";
 import "./App.css";
 import "bootstrap/dist/css/bootstrap.min.css";
 import "bootstrap-social/bootstrap-social.css";
-// import bootstrap js code
 import "typeface-roboto";
 import "font-awesome/css/font-awesome.min.css";
 
-var user = {
-  name: "Roshini",
-  loggedIn: false
-};
-
 var developers = {
-  "Roshini": {
-    "name": "Roshini Saravanakumar",
-    "role": "Frontend, UI/UX, Data Visualization, Databases",
-    "tools": "React.js, Bootstrap, Pandas, AWS, SQL, Node.js, APIs",
-    "github": "https://github.com/roshinis78",
-    "website": "https://roshinis78.github.io/my-website",
-    "picture": "./images/bt21_white_bg.png"
+  Roshini: {
+    name: "Roshini Saravanakumar",
+    role: "Frontend, UI/UX, Data Visualization, Databases",
+    tools: "React.js, Bootstrap, Pandas, AWS, SQL, Node.js, APIs",
+    github: "https://github.com/roshinis78",
+    website: "https://roshinis78.github.io/my-website",
+    picture: "./images/roshini-profile.jpg"
   },
-  "Eunice": {
-    "name": "Eunice Zhou",
-    "role": "fill this in",
-    "tools": "fill this in",
-    "github": "#",
-    "website": "#",
-    "picture": "./images/missing.png"
+  Eunice: {
+    name: "Eunice Zhou",
+    role: "fill this in",
+    tools: "fill this in",
+    github: "#",
+    website: "#",
+    picture: "./images/eunice-profile.jpg"
   },
-  "Matthew": {
-    "name": "Matthew Williams",
-    "role": "fill this in",
-    "tools": "fill this in",
-    "github": "#",
-    "website": "#",
-    "picture": "./images/missing.png"
+  Matthew: {
+    name: "Matthew Williams",
+    role: "fill this in",
+    tools: "fill this in",
+    github: "#",
+    website: "#",
+    picture: "./images/matthew-profile.jpg"
   },
-  "Sam": {
-    "name": "Samuel Dovgin",
-    "role": "fill this in",
-    "tools": "fill this in",
-    "github": "#",
-    "website": "#",
-    "picture": "./images/missing.png"
-  },
-}
+  Sam: {
+    name: "Samuel Dovgin",
+    role: "fill this in",
+    tools: "fill this in",
+    github: "#",
+    website: "#",
+    picture: "./images/sam-profile.jpg"
+  }
+};
 
 var topPicks = require("./data/topPicks.json");
 
 function App() {
   return (
     <div className="App">
-      <AboutPage></AboutPage>
+      <WelcomePage></WelcomePage>
+
+      <script src="https://unpkg.com/react-bootstrap@next/dist/react-bootstrap.min.js"/>
     </div>
   );
 }
@@ -87,16 +83,17 @@ class InfoBar extends React.Component {
         ></InfoCard>
       );
     }
-    if (!this.props.user.loggedIn) {
-      return (
-        <div className="container">
-          <div className="row">
-            <h1 className="text-left m-3">{this.props.title}</h1>
-            <div className="row flex-wrap justify-content-around m-1">{cards}</div>
+
+    return (
+      <div className="container">
+        <div className="row">
+          <h1 className="text-left m-3">{this.props.title}</h1>
+          <div className="row flex-wrap justify-content-around m-1">
+            {cards}
           </div>
         </div>
-      );
-    }
+      </div>
+    );
   }
 }
 
@@ -104,44 +101,9 @@ class WelcomePage extends React.Component {
   render() {
     return (
       <div>
-        <nav className="navbar navbar-expand-md navbar-light bg-light fixed-top pt-3">
-          <a className="navbar-brand mr-auto" href="#">
-            <img className="logo mr-3" src={require("./images/logo.png")}></img>
-            Illini Foodies
-          </a>
-          <button class='navbar-toggler' data-toggle='collapse' data-target="#nav-links">
-            <span class='navbar-toggler-icon'></span>
-          </button>
-          <ul className="navbar-nav collapse navbar-collapse justify-content-sm-end" id="nav-links">
-            <li className="nav-item active">
-              <a className="nav-link" href="#">
-                Home
-              </a>
-            </li>
-            <li className="nav-item">
-              <a className="nav-link" href="#">
-                About
-              </a>
-            </li>
-            <li className="nav-item">
-              <a className="nav-link" href="#">
-                Developers
-              </a>
-            </li>
-            <li className="nav-item">
-              <a className="nav-link" href="#">
-                Log In
-              </a>
-            </li>
-            <li className="nav-item">
-              <button className="btn btn-primary ml-2" href="#">
-                Sign Up
-              </button>
-            </li>
-          </ul>
-        </nav>
+        <Navbar active={[" active", "", "", "", "", "", ""]}></Navbar>
 
-        <header className="jumbotron foodie-header m-0">
+        <header className="custom-jumbotron foodie-header m-0">
           <h1 className="header-catch">hungry?</h1>
           <form className="form-inline justify-content-center align-items-end">
             <div className="input-group">
@@ -159,7 +121,7 @@ class WelcomePage extends React.Component {
           </form>
         </header>
 
-        <InfoBar user={user} title="Top Picks"></InfoBar>
+        <InfoBar title="Top Picks"></InfoBar>
       </div>
     );
   }
@@ -169,9 +131,12 @@ class AboutPage extends React.Component {
   render() {
     return (
       <div>
-        <header className="jumbotron bg-dark text-white">
+        <Navbar active={["", " active", "", "", "", "", ""]}></Navbar>
+
+        <header className="custom-jumbotron bg-dark">
           <h1>Meet the Developers!</h1>
         </header>
+
         <div className="row flex-wrap justify-content-center">
           <DeveloperCard dev={developers["Roshini"]}></DeveloperCard>
           <DeveloperCard dev={developers["Eunice"]}></DeveloperCard>
@@ -183,16 +148,92 @@ class AboutPage extends React.Component {
   }
 }
 
+class DemoPage extends React.Component {
+  redner() {
+    return (
+      <Navbar active={["", "", "", "", "", " active", ""]}></Navbar>
+    )
+  }
+}
+
+class Navbar extends React.Component {
+  render() {
+    return (
+      <nav className="navbar navbar-expand-md navbar-light bg-light fixed-top pt-3">
+        <a className="navbar-brand mr-auto" href="#">
+          <img className="logo mr-3" src={require("./images/logo.png")}></img>
+          Illini Foodies
+        </a>
+        <button
+          className="navbar-toggler"
+          data-toggle="collapse"
+          data-target="#nav-links"
+        >
+          <span className="navbar-toggler-icon"></span>
+        </button>
+        <ul
+          className="navbar-nav collapse navbar-collapse justify-content-sm-end"
+          id="nav-links"
+        >
+          <li className={"nav-item" + this.props.active[0]}>
+            <a className="nav-link" href="#">
+              Home
+            </a>
+          </li>
+          <li className={"nav-item" + this.props.active[1]}>
+            <a className="nav-link" href="#">
+              About
+            </a>
+          </li>
+          <li className={"nav-item" + this.props.active[2]}>
+            <a className="nav-link" href="#">
+              Developers
+            </a>
+          </li>
+          <li className={"nav-item" + this.props.active[3]}>
+            <a className="nav-link" href="#">
+              Log In
+            </a>
+          </li>
+          <li className={"nav-item" + this.props.active[4]}>
+            <a className="nav-link" href="#">
+              Demo
+            </a>
+          </li>
+          <li className={"nav-item" + this.props.active[5]}>
+            <button className="btn btn-facebook ml-2" href="#">
+              Sign Up
+            </button>
+          </li>
+        </ul>
+      </nav>
+    );
+  }
+}
+
 class DeveloperCard extends React.Component {
   render() {
     return (
       <div className="col-11 col-md-6 developer-card media">
-        <img src={require("./images/missing.png")} className="img-fluid thumbnail mr-3"></img>
+        <img
+          src={require("" + this.props.dev.picture)}
+          className="img-fluid thumbnail mr-3"
+        ></img>
         <div className="media-body">
           <h3>
-            {this.props.dev.name} 
-            <button className="btn btn-github"><a className="fa fa-github fa-lg text-white ml-1" href={this.props.dev.github}></a></button>
-            <button className="btn btn-twitter"><a className="fa fa-globe fa-lg text-white ml-1" href={this.props.dev.website}></a></button>
+            {this.props.dev.name}
+            <a
+              className="btn btn-github btn-social-icon m-1"
+              href={this.props.dev.github}
+            >
+              <span className="fa fa-github fa-lg fa-inverse"></span>
+            </a>
+            <a
+              className="btn btn-twitter btn-social-icon m-1"
+              href={this.props.dev.website}
+            >
+              <span className="fa fa-globe fa-lg fa-inverse"></span>
+            </a>
           </h3>
           <p>
             <i>{this.props.dev.role}</i>
@@ -213,42 +254,6 @@ class Rating extends React.Component {
       stars.push("⭐");
     }
     return <p>{stars}</p>;
-  }
-}
-
-class PlaceMap extends React.Component {
-  render() {
-    return (
-      <iframe
-        width="600"
-        height="450"
-        frameborder="0"
-        style={{ border: 0 }}
-        src="https://www.google.com/maps/embed/v1/place?q=place_id:ChIJv8lkCT_XDIgR7SK4MGhE3pQ&key=AIzaSyD13ilSA6TDU12vCSzzS3a1JY_PEpsgfqE"
-        allowfullscreen
-      ></iframe>
-    );
-  }
-}
-
-class PositionMap extends React.Component {
-  render() {
-    return (
-      <iframe
-        width="600"
-        height="450"
-        frameborder="0"
-        style={{ border: 0 }}
-        src={
-          "https://www.google.com/maps/embed/v1/view?zoom=8&center=" +
-          this.props.latitude +
-          "%2C" +
-          this.props.longitude +
-          "&key=AIzaSyD13ilSA6TDU12vCSzzS3a1JY_PEpsgfqE"
-        }
-        allowfullscreen
-      ></iframe>
-    );
   }
 }
 
