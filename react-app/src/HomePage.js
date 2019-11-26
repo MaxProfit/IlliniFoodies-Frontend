9945eaab46765a736c04e9bbe86b6cd22fa34959
Eunice Zhou <eunicexzhou@gmail.com>
2019-11-26 00:18:46 -0600

    Make small change to jumbotron UI

--- react-app/src/HomePage.js
+++ react-app/src/HomePage.js
@@ -1,13 +1,30 @@
 import React from "react";
 import InfoBar from "./InfoBar";
+import './HomePage.css';
+import Typist from 'react-typist';
 
 class HomePage extends React.Component {
   render() {
     return (
       <div>
-        <header className="custom-jumbotron foodie-header m-0">
-          <h1 className="header-catch">hungry?</h1>
-          <form className="form-inline w-100 justify-content-center align-items-end">
+        <header className="intro-header">
+          <div className="overlay"></div>
+          <div className="welcome-message">
+            <Typist className="typing-text">
+              <span>Welcome, Eunice</span>
+              <Typist.Delay ms={500} />
+              <br></br>
+              <span>what's up</span>
+            </Typist>
+            
+          </div>
+          
+
+          
+          
+          
+          {/* <h1 className="intro-catch">hungry?</h1> */}
+          {/* <form className="form-inline w-100 justify-content-center align-items-end">
             <div className="input-group w-75">
               <input
                 type="text"
@@ -20,7 +37,7 @@ class HomePage extends React.Component {
                 placeholder="tags (price, cuisine, i'm broke and hangry, etc.)"
               ></input>
             </div>
-          </form>
+          </form> */}
         </header>
 
         <InfoBar title="TOP PICKS NEAR YOU"></InfoBar>
